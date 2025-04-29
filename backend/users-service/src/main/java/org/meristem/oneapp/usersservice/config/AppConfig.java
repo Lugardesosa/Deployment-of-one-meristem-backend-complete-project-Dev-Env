34dd3c5998da45e9d4aecf1a2ee498fa2327f314
Kingsley Godwin <kingsleyog95@gmail.com>
2025-04-29 11:17:21 +0100

    Added open api config

--- backend/users-service/src/main/java/org/meristem/oneapp/usersservice/config/AppConfig.java
+++ backend/users-service/src/main/java/org/meristem/oneapp/usersservice/config/AppConfig.java
@@ -6,9 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
@@ -27,6 +32,12 @@ public class AppConfig {
 
     @Value("${one-app.server-url:http://localhost:20010/}")
     private String serverUrl;
+
+    @Value("${one-app.server-version}")
+    private String serverVersion;
+
+    @Value("${one-app.server-app-name}")
+    private String serverAppName;
 
     @Value("${one-app.email}")
     private String email;
@@ -71,8 +82,19 @@ public class AppConfig {
         server.setUrl(serverUrl.concat(contextPath));
         server.description("API Documentation");
 
-        Contact contact = new Contact().url(serverUrl).email(email).name("One App");
-        Info info = new Info().title("One App").version("1.0").contact(contact).description("This API exposes endpoints to manage users.");
-        return new OpenAPI().info(info).servers(List.of(server));
+        Contact contact = new Contact().url(serverUrl).email(email).name(serverAppName);
+        Info info = new Info().title(serverAppName).version(serverVersion).contact(contact).description("This API exposes endpoints to manage users.");
+
+        final String securitySchemeName = "OAuth2 Security";
+        return new OpenAPI().info(info).servers(List.of(server))
+                .components(new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                    .type(SecurityScheme.Type.OAUTH2)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                    .description("This API uses OAuth 2 with the implicit grant flow.")
+                    .flows(new OAuthFlows().password(new OAuthFlow().tokenUrl(serverUrl.concat("/api/users/oauth2/token"))))
+                )
+            ).security(List.of(new SecurityRequirement().addList(securitySchemeName)));
     }
 }
