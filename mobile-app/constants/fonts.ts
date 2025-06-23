0c22626bd14e7fb75219097aa41ab4ba7094ab07
lugardesosa100 <esosamumen@meristemng.com>
2025-06-23 10:19:47 +0100

    Delete mobile-app directory

--- mobile-app/constants/fonts.ts
+++
@@ -1,8 +0,0 @@
-export const FONTS = {
-  regular: "Poppins-Regular",
-  bold: "Poppins-Bold",
-  semiBold: "Poppins-SemiBold",
-  medium: "Poppins-Medium",
-  light: "Poppins-Light",
-  italic: "Poppins-Italic",
-};
