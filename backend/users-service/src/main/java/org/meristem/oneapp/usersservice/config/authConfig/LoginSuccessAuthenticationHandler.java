6ff5a8cc4eea4a2c89b22828e92344d393b99783
Kingsley Godwin <kingsleyog95@gmail.com>
2025-09-24 14:14:22 +0100

    Added product landing page check.

--- backend/users-service/src/main/java/org/meristem/oneapp/usersservice/config/authConfig/LoginSuccessAuthenticationHandler.java
+++ backend/users-service/src/main/java/org/meristem/oneapp/usersservice/config/authConfig/LoginSuccessAuthenticationHandler.java
@@ -61,7 +61,7 @@ public class LoginSuccessAuthenticationHandler implements AuthenticationSuccessH
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
 
         writeAccessAndRefreshToken(response, authentication);
         notifyUser(request, response, authentication);
@@ -73,9 +73,8 @@ public class LoginSuccessAuthenticationHandler implements AuthenticationSuccessH
             if (authentication.getPrincipal() instanceof UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) {
                 if (usernamePasswordAuthenticationToken.getPrincipal() instanceof AuthenticatedUser users) {
 
-//                    String ip = AppUtil.extractIp(request);
-                    // TODO: DELETE THE BELOW LINE AND UNCOMMENT THE ABOVE LINE
-                    String ip = "102.88.110.244";
+                    String ip = AppUtil.extractIp(request);
+
                     GeoIPDto location = loginService.getLocation(ip);
                     String deviceDetails = loginService.getDeviceDetails(AppUtil.getUserAgent(request));
 
@@ -93,8 +92,7 @@ public class LoginSuccessAuthenticationHandler implements AuthenticationSuccessH
                 }
             }
         } catch(Exception e) {
-            log.error("An error occurred verifying device or location");
-            throw new RuntimeException(e);
+            log.error("An error occurred verifying device or location: {}", e.getMessage());
         }
     }
 
