2d9ed6761c94d64c9b7c25fc869cc3dab5f277b5
Kingsley Godwin <kingsleyog95@gmail.com>
2025-10-02 14:50:15 +0100

    Deleted trusties-service

--- backend/trusties-service/src/main/java/org/meristem/oneapp/trustiesservice/domains/responses/SuccessResponse.java
+++
@@ -1,7 +0,0 @@
-package org.meristem.oneapp.trustiesservice.domains.responses;
-
-import lombok.Builder;
-
-@Builder
-public record SuccessResponse(String message, Boolean status, Long id) {
-}
--- backend/trusties-service/src/main/java/org/meristem/oneapp/trustiesservice/domains/responses/UpdateSelectionResponse.java
+++
@@ -1,7 +0,0 @@
-package org.meristem.oneapp.trustiesservice.domains.responses;
-
-import lombok.Builder;
-
-@Builder
-public record UpdateSelectionResponse(Integer successCount, String message) {
-}
