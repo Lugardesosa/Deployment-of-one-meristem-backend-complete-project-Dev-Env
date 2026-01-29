f61029658cd25ff6c4765734c7ef0a6a708d9063
Kingsley Godwin <65616098+realninety5@users.noreply.github.com>
2026-01-29 15:49:35 +0100

    Removed redundant code.

--- backend/users-service/src/main/java/org/meristem/oneapp/usersservice/UsersServiceApplication.java
+++ backend/users-service/src/main/java/org/meristem/oneapp/usersservice/UsersServiceApplication.java
@@ -1,16 +1,9 @@
 package org.meristem.oneapp.usersservice;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.meristem.oneapp.usersservice.constants.AppConstants;
 import org.meristem.oneapp.usersservice.constants.KafkaTopics;
-import org.meristem.oneapp.usersservice.models.AmlResult;
-import org.meristem.oneapp.usersservice.models.AmlSearch;
-import org.meristem.oneapp.usersservice.repositories.AmlResultRepository;
-import org.meristem.oneapp.usersservice.repositories.CustomRepository;
-import org.meristem.oneapp.usersservice.utils.AppUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -42,30 +35,8 @@ public class UsersServiceApplication {
 
     @Bean
     @Profile("!prod")
-    public CommandLineRunner warmUp(CacheManager cacheManager, KafkaTemplate<String, String> kafkaTemplate, AmlResultRepository amlResultRepository, CustomRepository customRepository) {
+    public CommandLineRunner warmUp(CacheManager cacheManager, KafkaTemplate<String, String> kafkaTemplate) {
 
-        AmlSearch search = AmlSearch.builder()
-                .searchType("")
-                .subjectReference("")
-                .vendor("")
-                .build();
-        customRepository.save(search);
-        ObjectNode jsonNode = AppUtil.getMapper().createObjectNode();
-        jsonNode.put("name", "Okah");
-        jsonNode.put("age", "22");
-        amlResultRepository.save(AmlResult.builder()
-                        .aliases("obi,dave")
-                        .name("Man dem")
-                        .dobs("1999")
-                        .countries("Nigeria,USA")
-                        .position(jsonNode)
-                        .vendorId("jkjka")
-                        .education(jsonNode)
-                        .searchId(search.getId())
-                        .confidenceScore(100)
-
-                .build());
-        log.info("All {}", amlResultRepository.findAll());
         return args -> {
             try {
                 requireNonNull(cacheManager.getCache(AppConstants.USERS_CACHE_NAME)).clear();
