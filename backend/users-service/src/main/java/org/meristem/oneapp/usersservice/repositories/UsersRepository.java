ee757d9934625e3d561b8d83891ef24df7330abd
Kingsley Godwin <65616098+realninety5@users.noreply.github.com>
2026-02-11 10:15:39 +0100

    Sorted investment intrument by id

--- backend/users-service/src/main/java/org/meristem/oneapp/usersservice/repositories/UsersRepository.java
+++ backend/users-service/src/main/java/org/meristem/oneapp/usersservice/repositories/UsersRepository.java
@@ -41,7 +41,7 @@ public interface UsersRepository extends BaseRepository<Users, Long> {
             "io.id AS o_iiid, io.name AS o_name, ioa.accessed AS o_accessed FROM users u LEFT JOIN user_profile up ON u.id = up.user_id " +
             "LEFT JOIN user_instrument ia ON ia.user_id = u.id LEFT JOIN investment_instruments ii ON ii.id = ia.instrument_id " +
             " LEFT JOIN investment_options io ON io.investment_id = ii.id LEFT JOIN investment_options_accessed ioa ON ioa.option_id = io.id " +
-            "WHERE u.id = :id AND u.password IS NOT NULL AND ii.status = 1 ", resultSetExtractorClass = UserResponseResultSetExtractor.class)
+            "WHERE u.id = :id AND u.password IS NOT NULL AND ii.status = 1 ORDER BY ii.id", resultSetExtractorClass = UserResponseResultSetExtractor.class)
     Optional<UsersResponse> findUserDetailsById(Long id);
 
     boolean existsByEmailOrPhoneNumber(String email, String phoneNumber);
