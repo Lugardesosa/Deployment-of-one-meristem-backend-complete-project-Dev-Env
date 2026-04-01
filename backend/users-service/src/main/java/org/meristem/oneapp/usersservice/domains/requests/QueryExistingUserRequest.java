1dd1aabd960ec7683a97c7a677a2d2c41a986b71
Kingsley Godwin <65616098+realninety5@users.noreply.github.com>
2026-04-01 13:56:04 +0100

    Added smile flow for existing and secondary users.

--- backend/users-service/src/main/java/org/meristem/oneapp/usersservice/domains/requests/QueryExistingUserRequest.java
+++ backend/users-service/src/main/java/org/meristem/oneapp/usersservice/domains/requests/QueryExistingUserRequest.java
@@ -7,5 +7,5 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.meristem.oneapp.usersservice.domains.enums.InvestmentInstruments;
 
-public record QueryExistingUserRequest(@Schema(pattern = "^[0-9]{11}$", example = "12345678901") @Pattern(regexp = "^[0-9]{11}$", message = "Pass a valid bvn") @NotBlank(message = "Pass a valid bvn") String idNumber, @NotNull(message = "Cannot be null") InvestmentInstruments investmentInstruments) {
+public record QueryExistingUserRequest(@Schema(pattern = "^[0-9]{11}$", example = "12345678901") @Pattern(regexp = "^[0-9]{11}$", message = "Pass a valid bvn") @NotBlank(message = "Pass a valid bvn") String idNumber) {
 }
