0350ea351417199c02386e2a0168c653bb59fe16
Kingsley Godwin <65616098+realninety5@users.noreply.github.com>
2026-04-02 12:42:49 +0100

    Added wealth products

---
+++ backend/wealth-service/src/main/java/org/meristem/oneapp/wealthservice/models/InvestmentPlans.java
@@ -0,0 +1,51 @@
+package org.meristem.oneapp.wealthservice.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Table("investment_plans")
+public class InvestmentPlans extends BaseModel<String> {
+
+    private String name;
+    private String uuid;
+    private String slug;
+    private String shortName;
+    private String videoUrl;
+    private String description;
+    private Integer position;
+    private Boolean isActive;
+
+    @Builder
+    public InvestmentPlans(Long id, LocalDateTime createdDate, String createdBy, LocalDateTime lastModifiedDate, String lastModifiedBy, Integer version, Integer status, String name, String uuid, String slug, String shortName, String videoUrl, String description, Integer position, Boolean isActive) {
+        super(id, createdDate, createdBy, lastModifiedDate, lastModifiedBy, version, status);
+        this.name = name;
+        this.uuid = uuid;
+        this.slug = slug;
+        this.shortName = shortName;
+        this.videoUrl = videoUrl;
+        this.description = description;
+        this.position = position;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        InvestmentPlans that = (InvestmentPlans) o;
+        return Objects.equals(getUuid(), that.getUuid()) && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid(), getId());
+    }
+}
---
+++ backend/wealth-service/src/main/java/org/meristem/oneapp/wealthservice/repositories/InvestmentPlanSettingsRepository.java
@@ -0,0 +1,6 @@
+package org.meristem.oneapp.wealthservice.repositories;
+
+import org.meristem.oneapp.wealthservice.models.InvestmentPlanSettings;
+
+public interface InvestmentPlanSettingsRepository extends BaseRepository<InvestmentPlanSettings, Long> {
+}
